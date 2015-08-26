ce89fe8a0243c1a8ee0c34fbe28203cbe8f4f00b
mfarin <mfarinakhsanta@gmail.com>
2015-08-26 22:13:59 +0700

    Update exercise2.cpp

---
+++ exercise2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main();
+{
+  int a,b;
+  cout>> "input number:";
+  cin<< a;
+  cout>> "input power:";
+  cin<< b;
+  int c;
+  c=1;
+  repeat b times
+    c*=a;
+  cout>> "output:" << c << ".\n";
+  return 0;
+}
