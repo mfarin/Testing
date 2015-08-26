f89c3a7403e067044f9e6818abfd8d94bdc98ffc
mfarin <mfarinakhsanta@gmail.com>
2015-08-26 21:43:50 +0700

    Update exercise1.cpp

---
+++ exercise1.cpp
@@ -0,0 +1,18 @@
+#include(iostream)
+using namespace std;
+int main()
+{
+  int a,b,c;
+  a=2;
+  b=-3;
+  c=1;
+  int hsl;
+  hsl=sqrt(b*b-4*a*c);
+    int x1,x2;
+    x1=(-b/2*a)+(hsl/2*a);
+    x2=(-b/2*a)-(hsl/2*a);
+    cout << "x1:";
+    cout << x1;
+    cout <<" x2:";
+    cout << x2;
+}
